3b1fc56a9df4a91eee813e73735b5ce7d7aee521
ArendtKacper <k.arendt@gridaly.com>
2023-07-05 08:42:01 +0200

    feat  prettier config

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,9 @@
-import { createServer } from "./server";
-import { logger } from "logger";
+import { createServer } from './server';
+import { logger } from 'logger';
 
 const port = process.env.PORT || 5001;
 const server = createServer();
 
 server.listen(port, () => {
-  logger.log("api index", `api running on ${port}`);
+	logger.log('api index', `api running on ${port}`);
 });
