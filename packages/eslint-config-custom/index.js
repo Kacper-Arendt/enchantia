f34311110b5d6f0d00dbcb4a6b26a4e84d67b798
ArendtKacper <k.arendt@gridaly.com>
2023-07-14 21:59:24 +0200

    feat: add eslint react/recommended

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,40 @@
 module.exports = {
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'next', 'turbo', 'prettier'],
 	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
+	plugins: ['@typescript-eslint', 'prettier'],
+	env: {
+		jest: true,
+		node: true,
+		browser: true,
+		es2021: true,
+	},
+	globals: {
+		document: true,
+		window: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	overrides: [
+		{
+			files: ['*.ts', '*.tsx'],
+			extends: [
+				'plugin:@typescript-eslint/eslint-recommended',
+				'plugin:@typescript-eslint/recommended',
+				'next',
+				'turbo',
+				'prettier',
+				'plugin:react/recommended',
+			],
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				project: 'tsconfig.json',
+				sourceType: 'module',
+			},
+			rules: {
+				'react/react-in-jsx-scope': 0,
+			},
+		},
+	],
 };
