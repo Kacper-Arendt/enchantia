1bb30616361fa22585197cae7255202e9c4a3dc2
ArendtKacper <k.arendt@gridaly.com>
2023-10-05 23:15:38 +0200

    feat story add dtos

--- apps/api/src/api/stories/stories.controller.ts
+++ apps/api/src/api/stories/stories.controller.ts
@@ -1,14 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 import { createStoryContentService, createStoryService, getStoryService } from 'src/api/stories/stories.services';
+import { createStoryContentDto, createStoryDto, getStoryContentDto, getStoryDto } from 'src/api/stories/stories.dto';
 
 export const createStory = async (req: Request, res: Response, next: NextFunction) => {
 	try {
 		const { name } = req.body;
 		const { userId } = req?.payload ?? {};
 
-		const data = await createStoryService({ ownerId: userId, name });
+		const data = await createStoryService(createStoryDto({ name, ownerId: userId }));
 
-		return res.status(200).json(data);
+		return res.status(200).json(getStoryDto(data));
 	} catch (e) {
 		next(e);
 	}
@@ -18,13 +19,11 @@ export const createStoryContent = async (req: Request, res: Response, next: Next
 	try {
 		const body = req.body;
 
-		const data = await createStoryContentService(body);
+		const data = await createStoryContentService(createStoryContentDto(body));
+		const updatedStory = await getStoryService({ id: data?.storyId });
+		const translations = updatedStory?.translations?.map((el) => getStoryContentDto(el)) ?? [];
 
-		if (data) {
-			const updatedStory = await getStoryService({ id: data?.storyId });
-
-			return res.status(200).json(updatedStory);
-		}
+		if (data && updatedStory) return res.status(200).json({ ...createStoryDto(updatedStory), translations });
 	} catch (e) {
 		next(e);
 	}
--- apps/api/src/api/stories/stories.dto.ts
+++ apps/api/src/api/stories/stories.dto.ts
@@ -1,7 +1,37 @@
-import { Story } from 'database';
+import { Story, StoryTranslation } from 'database';
 
-export const storyDto = (story: Story): Story => ({
-	id: story?.id,
+type CreateStoryDto = Pick<Story, 'ownerId' | 'name'>;
+
+export const createStoryDto = (story: CreateStoryDto): CreateStoryDto => ({
 	ownerId: story?.ownerId,
 	name: story?.name,
+});
+
+export const getStoryDto = (story: Story): Partial<Story> => ({
+	id: story.id,
+	name: story.name,
+	ownerId: story.ownerId,
+});
+
+type CreateStoryContentDtoType = Pick<StoryTranslation, 'content' | 'storyId' | 'language' | 'description' | 'title'>;
+
+export const createStoryContentDto = (translation: CreateStoryContentDtoType): CreateStoryContentDtoType => ({
+	title: translation.title,
+	content: translation.content,
+	storyId: translation.storyId,
+	language: translation.language,
+	description: translation.description,
+});
+
+export const getStoryContentDto = (
+	translation: StoryTranslation,
+): Pick<StoryTranslation, 'title' | 'content' | 'storyId' | 'language' | 'description' | 'id' | 'status' | 'createdAt'> => ({
+	title: translation.title,
+	content: translation.content,
+	storyId: translation.storyId,
+	language: translation.language,
+	description: translation.description,
+	id: translation.id,
+	status: translation.status,
+	createdAt: translation.createdAt,
 });
