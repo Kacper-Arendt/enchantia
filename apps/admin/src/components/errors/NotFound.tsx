7061264beb5e8cad8c207577ad297d5c98a03740
ArendtKacper <k.arendt@gridaly.com>
2023-10-21 20:12:16 +0200

    feat add base styling

---
+++ apps/admin/src/components/errors/NotFound.tsx
@@ -0,0 +1,17 @@
+import { Link } from '@tanstack/router';
+import { t } from 'i18next';
+
+// STYLES
+import styles from 'src/components/errors/styles.scss';
+
+// UTILS
+
+export const NotFound = () => (
+	<div className={styles.notFound}>
+		<h1 className={styles.title}>{t('errors.pageNotFound')}</h1>
+		<p className={styles.description}>{t('errors.pageNotFound-desc')}</p>
+		<Link href="/" className={styles.goBack}>
+			{t('general.goBackToHomePage')}
+		</Link>
+	</div>
+);
--- apps/admin/src/components/layouts/PrivateLayout.tsx
+++ apps/admin/src/components/layouts/PrivateLayout.tsx
@@ -1,7 +1,51 @@
+import { ErrorBoundary } from 'react-error-boundary';
+import { useRef, useState } from 'react';
+import clsx from 'clsx';
+import { RxHamburgerMenu } from 'react-icons/rx';
 import { Outlet } from '@tanstack/router';
+import { AiOutlineClose } from 'react-icons/ai';
 
-export const PrivateLayout = () => (
-	<div>
-		<Outlet />
-	</div>
-);
+// HOOKS
+import { useOnClickOutside } from 'src/utils/useClickOutside';
+
+// COMPONENTS
+import { FallbackError } from 'src/components/errors';
+
+// STYLES
+import styles from 'src/components/layouts/Styles.module.scss';
+
+export const PrivateLayout = () => {
+	const ref = useRef<HTMLDivElement>(null);
+	const [openAside, setOpenAside] = useState(false);
+	useOnClickOutside(ref, () => setOpenAside(false));
+
+	return (
+		<ErrorBoundary FallbackComponent={FallbackError}>
+			<div className={styles.privateLayout}>
+				<ErrorBoundary FallbackComponent={FallbackError}>
+					<header className={styles.header}>
+						<button type="button" onClick={() => setOpenAside(true)} className={styles.toggleNavButton}>
+							<RxHamburgerMenu />
+						</button>
+					</header>
+				</ErrorBoundary>
+
+				<ErrorBoundary FallbackComponent={FallbackError}>
+					<aside className={clsx(styles.aside, { [styles.openAside]: openAside })} ref={ref}>
+						<div className={styles.asideHeader}>
+							<button type="button" onClick={() => setOpenAside(false)} className={styles.toggleNavButton}>
+								<AiOutlineClose />
+							</button>
+						</div>
+					</aside>
+				</ErrorBoundary>
+
+				<ErrorBoundary FallbackComponent={FallbackError}>
+					<main>
+						<Outlet />
+					</main>
+				</ErrorBoundary>
+			</div>
+		</ErrorBoundary>
+	);
+};
