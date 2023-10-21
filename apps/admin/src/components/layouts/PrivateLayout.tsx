d34e3edbfd1f5ad5df0173deb9623f8caf7d0ba0
ArendtKacper <k.arendt@gridaly.com>
2023-10-21 20:22:43 +0200

    feat add app name

--- apps/admin/src/components/layouts/PrivateLayout.tsx
+++ apps/admin/src/components/layouts/PrivateLayout.tsx
@@ -14,6 +14,9 @@ import { FallbackError } from 'src/components/errors';
 // STYLES
 import styles from 'src/components/layouts/Styles.module.scss';
 
+// CONFIG
+import { envs } from 'src/config/envs';
+
 export const PrivateLayout = () => {
 	const ref = useRef<HTMLDivElement>(null);
 	const [openAside, setOpenAside] = useState(false);
@@ -27,12 +30,14 @@ export const PrivateLayout = () => {
 						<button type="button" onClick={() => setOpenAside(true)} className={styles.toggleNavButton}>
 							<RxHamburgerMenu />
 						</button>
+						{envs.appName && <h3 className={styles.appName}>{envs.appName}</h3>}
 					</header>
 				</ErrorBoundary>
 
 				<ErrorBoundary FallbackComponent={FallbackError}>
 					<aside className={clsx(styles.aside, { [styles.openAside]: openAside })} ref={ref}>
 						<div className={styles.asideHeader}>
+							{envs.appName && <h3 className={styles.appName}>{envs.appName}</h3>}
 							<button type="button" onClick={() => setOpenAside(false)} className={styles.toggleNavButton}>
 								<AiOutlineClose />
 							</button>
--- apps/admin/src/config/envs.ts
+++ apps/admin/src/config/envs.ts
@@ -1,4 +1,5 @@
 export const envs = {
 	apiUrl: import.meta.env.VITE_API_URL,
 	isDev: import.meta.env.VITE_API_URL === 'development',
+	appName: import.meta.env.VITE_APP_NAME,
 };
